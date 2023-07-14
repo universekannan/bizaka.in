560b02aff49f35d5b92cad45c123f11bf40bf8d1
galaxysaravanan <saravananperumal2014@gmail.com>
2023-07-14 10:50:27 +0530

    profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,8 +11,30 @@ class ProfileController extends Controller
     public function profile()
      {
       $userid = Auth::user()->id; 
-      $proile = DB::table('users')->where('id','=', $userid)->get();
+      $profile = DB::table('users')->where('id','=', $userid)->get();
      return view('users/profile', compact('profile'));
 
     }
+    public function updateprofile(Request $request){
+        $userid = Auth::user()->id; 
+        $updateprofile = DB::table( 'users' )->where('id',$userid)->update([
+          'name'       => $request->name,
+          'aadhar_no'  => $request->aadhar_no,
+          'phone'      => $request->phone,
+          'email'      => $request->email,
+          'gender'     => $request->gender,
+          'address'    => $request->address,
+          'upi'        => $request->upi,
+        ]);
+      
+        $qrcode ="";
+        if($request->payment_qr_oode != null){
+         $qrcode = $userid.'.'.$request->file('payment_qr_oode')->extension(); 
+         $filepath = public_path('upload'.DIRECTORY_SEPARATOR.'qrcodeimg'.DIRECTORY_SEPARATOR);
+         move_uploaded_file($_FILES['payment_qr_oode']['tmp_name'], $filepath.$qrcode);
+         $sql = "update users set payment_qr_oode='$qrcode' where id = $userid";
+           DB::update(DB::raw($sql));
+       }
+       return redirect('dashboard')->with('success', 'Edit User Successfully ... !');
+      }
 }
