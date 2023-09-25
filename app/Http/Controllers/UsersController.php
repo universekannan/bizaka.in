ad86450f33f0663b04723f53b82982085697438b
Galaxy Kannan <universekannan@gmail.com>
2023-09-25 12:19:37 +0530

    kannan

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,14 +6,14 @@ use Hash;
 use Auth;
 
 class UsersController extends Controller
- {
+{
 
     public function members() {
         $parent_id = Auth::user()->id;
         if ( $parent_id == 1 )
-        $members = DB::table( 'users' )->where( 'usertype_id', 3 )->orderBy( 'id', 'Asc' )->get();
+            $members = DB::table( 'users' )->where( 'usertype_id', 3 )->orderBy( 'id', 'Asc' )->get();
         else
-        $members = DB::table( 'users' )->where( 'parent_id', $parent_id )->orderBy( 'id', 'Asc' )->get();
+            $members = DB::table( 'users' )->where( 'parent_id', $parent_id )->orderBy( 'id', 'Asc' )->get();
 
         return view( 'users/index', compact( 'members' ) );
     }
@@ -43,8 +43,8 @@ class UsersController extends Controller
             'phone' => $request->phone,
             'address' => $request->address,
             'updated_at' =>  date( 'Y-m-d H:i:s' ),
-          ]);
-          return redirect( '/members' )->with( 'success', 'Member added successfully' );
+        ]);
+        return redirect( '/members' )->with( 'success', 'Member added successfully' );
     }
 
     public function purchase($id){
@@ -52,85 +52,56 @@ class UsersController extends Controller
         return view( 'users/purchase', compact( 'purchases','id' ) );
     }
 
-    public function addproductd( Request $request ) {
+    public function addproduct( Request $request ) {
         $log_id = Auth::user()->id;
         $amount = $request->amount;
+        $paydate = date('Y-m-d');
+        $time = date("H:i:s");
+        $member_id = $request->member_id;
         DB::table( 'purchase' )->insert( [
-            'member_id' => $request->member_id,
+            'member_id' => $member_id,
             'amount' => $amount,
             'purchase_date' =>  date( 'Y-m-d' ),
             'added_datetime' =>  date( 'Y-m-d H:i:s' ),
             'log_id' => $log_id,
-        ] );
-		$member_id = $request->member_id;
-		  $sql = "select referral_id from users where id = $member_id";
-		  $result = DB::select(DB::raw($sql));
-	    $referral_id = $result[0]->referral_id;
-
-        $paydate = date('Y-m-d');
-    $time = date("H:i:s");
-    $member_id = 0;
-    $child_id = 0;
-    $parent_id = 0;
-    $wallet = 0;
-    $sql = "select * from users where referral_id = '$referral_id'";
-    $result = DB::select(DB::raw($sql));
-    if(count($result) > 0){
-      $member_id = $result[0]->id;
-      $child_id = $result[0]->id;
-      $parent_id = $result[0]->referral_id;
-      
-    }
-    $ownamount = ceil($amount * 10 / 100);
-    $ad_info = "Own Amount";
-    $service_status = "In Payment";
-    $sql = "insert into payment (log_id,from_id,to_id,amount,ad_info,service_status,time,paydate) values ('$log_id','$member_id','$member_id', '$ownamount','$ad_info', '$service_status','$time','$paydate')";
-    DB::insert(DB::raw($sql));
-    $sql = "update users set wallet = wallet + $amount where id = $member_id";
-    DB::update(DB::raw($sql));
-    $first_parent = true;
-    do{
-      $sql = "select * from users where id = $child_id";
-      $result = DB::select(DB::raw($sql));
-      if(count($result) > 0){
-        $child_id = $result[0]->id;
-        $parent_id = $result[0]->referral_id;
-
-      }
-
-      if($parent_id != 1 && $first_parent == true) {
-        $amount = ceil($amount * 2 / 100);
-        
-      }
-       if($parent_id != 1 && $first_parent == false) {
-        $amount = ceil($amount * 1 / 100);
-        $bal = $amount;
-       
-      }
-      
-      if($parent_id == 1 && $first_parent == true){
-        $amount = ceil($amount * 1 / 100);
-        $bal = $amount;
-       
-       
-      }
-      if($parent_id == 1 && $first_parent == false){
-        $amount = ceil($amount * 1 / 100);
-        $amount = $amount;
-      } 
-
-
-     
-      $first_parent = false;
-      $ad_info = "Activation";
-      $service_status = "In Payment";
-      $sql = "insert into payment (log_id,from_id,to_id,amount,ad_info,service_status,time,paydate) values ('$log_id','$child_id','$parent_id', '$amount','$ad_info', '$service_status','$time','$paydate')";
-      DB::insert(DB::raw($sql));
-      $sql = "update users set wallet = wallet + $amount where id = $parent_id";
-      DB::update(DB::raw($sql));
-      $child_id = $parent_id;
-    }while($parent_id != 1);
-
-        return redirect( "/purchase/$member_id" )->with( 'success', 'Member added successfully' );
+        ]);
+        $paycount = 0;
+        $points = round($amount * 10 / 100);
+        $ad_info = "In Payment";
+        $service_status = "In Payment";
+        $sql = "insert into payment (log_id,from_id,to_id,amount,ad_info,service_status,time,paydate) values ('$log_id','$member_id','$member_id', '$points','$ad_info', '$service_status','$time','$paydate')";
+        DB::insert(DB::raw($sql));
+        $sql = "update users set wallet = wallet + $points where id = $member_id";
+        DB::update(DB::raw($sql));
+        $paycount++;
+        $user_id = $member_id;
+        $percentage = 2;
+        while(true){
+            $sql = "select referral_id from users where id = $user_id";
+            $result = DB::select(DB::raw($sql));
+            if(count($result)>0){
+                $user_id = $result[0]->referral_id;
+                if($user_id != 0 && $paycount <= 7){
+                    if($paycount == 1){
+                        $percentage = 2;
+                    }else if($paycount == 2){
+                        $percentage = 1;
+                    }else{
+                        $percentage = 0.5;
+                    }
+                    $points = round($amount * $percentage / 100);
+                    $sql = "insert into payment (log_id,from_id,to_id,amount,ad_info,service_status,time,paydate) values ('$log_id','$member_id','$user_id', '$points','$ad_info', '$service_status','$time','$paydate')";
+                    DB::insert(DB::raw($sql));
+                    $sql = "update users set wallet = wallet + $points where id = $user_id";
+                    DB::update(DB::raw($sql));
+                }else{
+                    break;
+                }
+                $paycount++;
+            }else{
+                break;
+            }
+        }    
+        return redirect( "/purchase/$member_id" )->with( 'success', 'Purchase added successfully');
     }
 }
--- resources/views/users/purchase.blade.php
+++ resources/views/users/purchase.blade.php
@@ -40,142 +40,70 @@
                         </div>
                         @endif
                         <div class="table-responsive">
-                        <table id="example2" class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>S No</th>
-                                    <th>Purchase Date</th>
-                                    <th>Price</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($purchases as $key => $purchase)
-                                <tr>
-                                    <td>{{ $key + 1 }}</td>
-                                    <td>{{ $purchase->purchase_date }}</td>
-                                    <td>{{ $purchase->amount }}</td>
+                            <table id="example2" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>S No</th>
+                                        <th>Purchase Date</th>
+                                        <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($purchases as $key => $purchase)
+                                    <tr>
+                                        <td>{{ $key + 1 }}</td>
+                                        <td>{{ $purchase->purchase_date }}</td>
+                                        <td>{{ $purchase->amount }}</td>
                                     </tr>
                                     @endforeach
                                 </tbody>
                             </table>
                         </div>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-    <div class="modal fade" id="addproducts">
-        <form action="{{ url('/addproduct') }}" method="post">
-            {{ csrf_field() }}
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">Add Products</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                      
-                         <input type="hidden" value="{{ $id }}" name="member_id"> 
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group row">
-                                        <label for="amount" class="col-sm-4 col-form-label"><span
-                                            style="color:red">*</span>Amount</label>
-                                            <div class="col-sm-8">
-                                                <input required="required" type="text" class="form-control number"
-                                                name="amount" id="amount" maxlength="10"
-                                                placeholder="Amount">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                    <div class="modal-footer justify-content-between">
-                                        <button type="button" class="btn btn-default"
-                                        data-dismiss="modal">Close</button>
-                                        <input class="btn btn-primary" type="submit" value="Submit" />
-                                    </div>
+    </div>
+</section>
+<div class="modal fade" id="addproducts">
+    <form action="{{ url('/addproduct') }}" method="post">
+        {{ csrf_field() }}
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Add Purchase</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                   <input type="hidden" value="{{ $id }}" name="member_id"> 
+                   <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group row">
+                            <label for="amount" class="col-sm-4 col-form-label"><span
+                                style="color:red">*</span>Amount</label>
+                                <div class="col-sm-8">
+                                    <input required="required" type="text" class="form-control number"
+                                    name="amount" id="amount" maxlength="10"
+                                    placeholder="Amount">
                                 </div>
                             </div>
                         </div>
-                    </form> 
-                </div>   
-
-                <div class="modal fade" id="editmember">
-                    <form action="{{ url('/updatemember') }}" method="post">
-                        {{ csrf_field() }}
-                        <input type="hidden" name="id" id="member_id">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h4 class="modal-title">Edit Member</h4>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group row">
-                                                <label for="name" class="col-sm-4 col-form-label"><span
-                                                    style="color:red">*</span>Name</label>
-                                                    <div class="col-sm-8">
-                                                        <input required="required" type="text" class="form-control"
-                                                        name="name" id="editname" maxlength="30"
-                                                        placeholder="Full Name">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="form-group row">
-                                                    <label for="phone" class="col-sm-4 col-form-label"><span
-                                                        style="color:red">*</span>Phone</label>
-                                                        <div class="col-sm-8">
-                                                            <input required="required" type="text" class="form-control number"
-                                                            name="phone" id="editphone" maxlength="10"
-                                                            placeholder="Phone Number">
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                           
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group row">
-                                                <label for="address" class="col-sm-4 col-form-label"><span
-                                                    style="color:red">*</span>Address</label>
-                                                    <div class="col-sm-8">
-                                                        <textarea required="required" class="form-control"
-                                                        name="address" id="editaddress" maxlength="200" rows="2"
-                                                        placeholder="Address .."></textarea>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                                <div class="modal-footer justify-content-between">
-                                                    <button type="button" class="btn btn-default"
-                                                    data-dismiss="modal">Close</button>
-                                                    <input class="btn btn-primary" type="submit" value="Submit" />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form> 
-                            </div>   
-
-                            @endsection
-                            @push('page_scripts')
-                            <script>
-                                function edit_member(id,name,phone,address) {
-                                    $("#member_id").val(id);
-                                    $("#editname").val(name);
-                                    $("#editphone").val(phone);
-                                    $("#editaddress").val(address);
-                                    $("#editmember").modal("show");
-                                }
-                            </script>
-                            @endpush
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default"
+                        data-dismiss="modal">Close</button>
+                        <input class="btn btn-primary" type="submit" value="Submit" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form> 
+</div>   
+@endsection
+@push('page_scripts')
+<script>
+    
+</script>
+@endpush
